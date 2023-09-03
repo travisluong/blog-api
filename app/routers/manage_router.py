1a68581cd8f79693177456f239c3c400494a7212
Travis Luong <travis@travisluong.com>
2023-09-03 16:47:13 -0700

    load fake data

--- app/routers/manage_router.py
+++ app/routers/manage_router.py
@@ -1,8 +1,48 @@
-from fastapi import APIRouter
+import random
+
+from fastapi import APIRouter, BackgroundTasks
+from faker import Faker
+import bcrypt
+
 
 from app.db import get_conn
 
 router = APIRouter(prefix="/manage")
+
+fake = Faker()
+
+
+def load_fake_data_task():
+    print("executing load fake data")
+    password = b"blogapi123"
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    with get_conn() as conn:
+        conn.execute(
+            "insert into users (email, password, is_admin) values (%s, %s, %s)",
+            ["admin@example.com", hashed, True],
+        )
+
+        for i in range(10):
+            conn.execute(
+                "insert into users (email, password) values (%s, %s)",
+                [fake.email(), hashed],
+            )
+
+        for category in ["react", "fastapi", "springboot", "nextjs"]:
+            conn.execute("insert into categories (name) values (%s)", [category])
+
+        for i in range(20):
+            post = {
+                "user_id": 1,
+                "category_id": random.choice([1, 2, 3]),
+                "title": fake.sentence(nb_words=8),
+                "content": fake.paragraph(nb_sentences=5),
+                "status": random.choice(["draft", "public", "private"]),
+            }
+            conn.execute(
+                "insert into posts (user_id, category_id, title, content, status) values (%(user_id)s, %(category_id)s, %(title)s, %(content)s, %(status)s)",
+                post,
+            )
 
 
 @router.get("/ping")
@@ -11,3 +51,9 @@ def ping():
         record = conn.execute("select 1").fetchone()
         print(record)
         return "pong"
+
+
+@router.get("/load-fake-data")
+def load_fake_data(background_tasks: BackgroundTasks):
+    background_tasks.add_task(load_fake_data_task)
+    return {"message": "load fake data running in background"}
