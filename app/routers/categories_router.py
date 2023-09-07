1b05f145c3c5dfa47d1d50b2bd6d23772dd218a3
Travis Luong <travis@travisluong.com>
2023-09-06 20:40:27 -0700

    create category endpoint

--- app/routers/categories_router.py
+++ app/routers/categories_router.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from psycopg.rows import class_row
 from pydantic import BaseModel
 
@@ -16,7 +16,32 @@ class Category(BaseModel):
     updated_at: datetime
 
 
+class CategoryReq(BaseModel):
+    name: str
+
+
+class CategoryRes(BaseModel):
+    category_id: int
+    name: str
+    created_at: datetime
+    updated_at: datetime
+
+
 @router.get("/")
 def get_categories(conn: DBDep):
     with conn.cursor(row_factory=class_row(Category)) as cur:
         return cur.execute("select * from categories").fetchall()
+
+
+@router.post("/")
+def create_category(conn: DBDep, category_req: CategoryReq):
+    with conn.cursor(row_factory=class_row(CategoryRes)) as cur:
+        is_exists = cur.execute(
+            "select * from categories where name = %s", [category_req.name]
+        ).fetchone()
+        if is_exists:
+            return HTTPException(status_code=400, detail="category already exists")
+        record = cur.execute(
+            "insert into categories (name) values (%s) returning *", [category_req.name]
+        ).fetchone()
+        return record
