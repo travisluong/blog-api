5ca77d5ecf5b4c30ee4ee1392c20bdec1cbb9872
Travis Luong <travis@travisluong.com>
2023-09-08 22:09:27 -0700

    category param

--- app/routers/posts_router.py
+++ app/routers/posts_router.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from psycopg.rows import class_row
 from pydantic import BaseModel
 
@@ -7,6 +7,11 @@ from app.dependencies import DBDep, JwtDep
 
 
 router = APIRouter(prefix="/posts")
+
+
+class Category(BaseModel):
+    category_id: int
+    name: str
 
 
 class Post(BaseModel):
@@ -22,12 +27,30 @@ class Post(BaseModel):
 
 
 @router.get("/")
-def get_posts(conn: DBDep, jwt_payload: JwtDep):
-    with conn.cursor(row_factory=class_row(Post)) as posts_cur:
+def get_posts(conn: DBDep, jwt_payload: JwtDep, category: str | None = None):
+    with (
+        conn.cursor(row_factory=class_row(Category)) as categories_cur,
+        conn.cursor(row_factory=class_row(Post)) as posts_cur,
+    ):
         if not jwt_payload:
             sql = "select * from posts where status = 'public'"
         elif jwt_payload["is_admin"]:
-            sql = "select * from posts"
+            sql = "select * from posts where 1 = 1"
         elif not jwt_payload["is_admin"]:
             sql = "select * from posts where status != 'draft'"
-        return posts_cur.execute(sql).fetchall()
+
+        params = {}
+
+        if category:
+            c = categories_cur.execute(
+                "select * from categories where name = %s", [category]
+            ).fetchone()
+            if not c:
+                raise HTTPException(status_code=404, detail="category not found")
+            else:
+                sql += " and category_id = %(category_id)s"
+                params["category_id"] = c.category_id
+
+        print(sql)
+        print(params)
+        return posts_cur.execute(sql, params).fetchall()
